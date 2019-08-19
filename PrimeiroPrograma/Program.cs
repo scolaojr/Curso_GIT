5d7b79d457c4c24b3fe83ff5c30c51e1629b3dec
Sergio Colao <scolaojr@gmail.com>
2019-08-19 06:13:41 -0300

    Terceiro Commit

--- PrimeiroPrograma/Program.cs
+++ PrimeiroPrograma/Program.cs
@@ -6,7 +6,8 @@ namespace PrimeiroPrograma
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
             Console.ReadLine();
         }
     }
